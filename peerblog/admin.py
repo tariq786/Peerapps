ca29d62cfc1a1a471b59a51a98f8b69b69856f92
Emeth <emeth@Seans-MacBook-Pro.local>
2015-04-25 18:36:06 -0500

    added peerblog to django admin panel

--- peerblog/admin.py
+++ peerblog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Blog
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('key', 'address_from', 'block_index', 'tx_id')
+    search_fields = ('key', 'address_from', 'block_index', 'tx_id')
+admin.site.register(Blog, BlogAdmin)
+
+from models import Subscription
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('address', 'time')
+    search_fields = ('address',)
+admin.site.register(Subscription, SubscriptionAdmin)
--- setup/admin.py
+++ setup/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 
 from models import BlockchainScan
 class BlockchainScanAdmin(admin.ModelAdmin):
-    #raw_id_fields = ('field1',)
     list_display = ('id',)
     search_fields = ('id',)
 admin.site.register(BlockchainScan, BlockchainScanAdmin)
 
 from models import MemPoolScan
 class MemPoolScanAdmin(admin.ModelAdmin):
-    #raw_id_fields = ('field1',)
     list_display = ('id',)
     search_fields = ('id',)
 admin.site.register(MemPoolScan, MemPoolScanAdmin)
